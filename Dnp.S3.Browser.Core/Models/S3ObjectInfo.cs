66fa354b128f3e632bf901d0d21ad41e94eab6b6
sdh <ccc_sdh@comcast.net>
2026-02-20 10:56:48 -0500

    Revert "Merge more updates"
    This reverts commit 30d1578d62ed87c9dd00a2409b9ef8ae2c5e03df.

--- Dnp.S3.Browser.Core/Models/S3ObjectInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Dnp.S3.Browser.Core.Models;
-
-public record S3ObjectInfo
-{
-    public string Key { get; init; } = string.Empty;
-    public bool IsFolder { get; init; }
-    public long? Size { get; init; }
-    public DateTime? LastModified { get; init; }
-}
